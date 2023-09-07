9ab6bb25d8f7d27d27479dd7e329e1f13b8539bc
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-09-07 14:17:51 +0300

    receive the file name, type, and size in bytes within the JSON response

--- index.js
+++ index.js
@@ -18,3 +18,13 @@ const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
+
+let multer = require('multer');
+
+app.post('/api/fileanalyse', multer().single('upfile'), function(req, res){
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+})
