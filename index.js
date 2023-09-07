a5efbf5a86e105d9793b94a305f58805aa0d3ec6
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-09-07 13:06:25 +0300

    new file:   index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var cors = require('cors');
+require('dotenv').config()
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
